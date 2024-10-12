82b75a4a151450e8f928f492c1d8c89ffc473527
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-12 06:22:17 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,20 +1,20 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -n --userprefix concierge --userdomain de.mo
+sf demoutil org create scratch -f config/project-scratch-def.json -d 2 -s -p concierge -e de.mo
 sfdx force:package:install --package 04t4P000002qm6f --noprompt --wait 50
 
 # to get access to audit field create
-sfdx force:source:deploy -p securityStuff/
+sf project deploy start -p securityStuff/
 # give the admin knowledge permission
-sfdx force:data:record:update -s User -w "Name='User User'" -v "UserPermissionsKnowledgeUser=true"
+sf data update record -s User -w "Name='User User'" -v "UserPermissionsKnowledgeUser=true"
 
-sfdx force:source:push
-sfdx force:user:permset:assign -n solutions
+sf project deploy start
+sf org assign permset -n solutions
 
 # add a background option to settings
-sfdx force:data:record:create -s cncrgdemo2__Background_Settings__c -v "cncrgdemo2__Background_File_Type__c=Image cncrgdemo2__File_Location__c=newBackground Name=NewBackground"
-sfdx force:data:bulk:upsert -f data/Knowledge__kav.csv -i id -s Knowledge__kav --wait 30
+sf data create record -s cncrgdemo2__Background_Settings__c -v "cncrgdemo2__Background_File_Type__c=Image cncrgdemo2__File_Location__c=newBackground Name=NewBackground"
+sf data bulk upsert -f data/Knowledge__kav.csv -i id -s Knowledge__kav --wait 30
 
-sfdx force:apex:execute -f scripts/conciergeSetup.cls
-sfdx automig:load -d data2
+sf apex run -f scripts/conciergeSetup.cls
+sf automig load -d data2
 
 sfdx shane:data:favorite -o Knowledge__kav -w "title='Flickering Monitor'"
 sfdx shane:listview:favorite -t Open_IT_Tickets -o Case
@@ -22,4 +22,4 @@ sfdx shane:listview:favorite -t published_articles -o Knowledge__kav -l "Publish
 sfdx shane:tab:favorite -t cncrgdemo2__Cadalys_Concierge -l "Concierge in Lightning"
 sfdx shane:tab:favorite -t cncrgdemo2__Concierge_Settings -l "Concierge Settings"
 
-sfdx force:org:open -p lightning/n/cncrgdemo2__Cadalys_Concierge
+sf org open -p lightning/n/cncrgdemo2__Cadalys_Concierge
